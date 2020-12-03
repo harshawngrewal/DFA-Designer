e3c2666b7fee2fc7794c5f342f12818eb577989f
Harshawn Grewal <harshawngrewal010@gmail.com>
2020-12-02 19:49:14 -0500

    added CircleCommand

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
 
-// here is the code for the controller
-function canvasController(canvasView, canvasModel) {
-  this.canvasView = canvasView;
-  this.canvasModel = canvasModel;
-}
-
-canvasController.prototype.updateModel = function updateModel() {
-
-}
-
-// here is the code for the model
-function canvasModel() {
-  this.observers = [];
-  observers.push("wassup")
-}
-
-canvasModel.prototype.removeObserver = function removeObserver(object){
-  for(i = 0; i< this.observers.length; i ++){
-  if (this.observers[i] == object) {
-    this.observer.pop(i);
+// --------------here is the code for the controller --------
+class canvasController {
+  constructor(canvasModel) {
+    this.canvasModel = canvasModel;
   }
+  updateModel() {
+  }
+}
+
+
+//------------- Here is the code for the model------------
+
+class canvasModel {
+  constructor() {
+    this.canvasView = document.getElementById("canvas");
+    this.observers = [];
+  }
+  removeObserver(object) {
+    for (i = 0; i < this.observers.length; i++) {
+      if (this.observers[i] == object) {
+        this.observers.pop(i);
+      }
+
+    }
+  }
+  addObserver(object) {
+    this.observers.push(object);
+    object.execute(); // every command will have an execute method
+
+  }
+  clear() {
+    canvasView.clearRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
+
+
+// --------- CirlceComamnd -------- //
+class CircleCommand {
+  constructor(xcord, ycord) {
+    this.canvas = document.getElementById("canvas");
+    this.ctx = this.canvas.getContext('2d');
+    this.xcord = xcord;
+    this.ycord = ycord;
+  }
+}
+CircleCommand.prototype.execute = function execute(){
+  this.ctx.beginPath();
+  this.ctx.arc(this.xcord, this.ycord, 35, 0, 2 * Math.PI);
+  this.ctx.stroke();
 
 }
-}
+
+// let circle = new CircleCommand(800, 50);
+// circle.execute();
+
+
+
+
+
+
+
+
 
 
